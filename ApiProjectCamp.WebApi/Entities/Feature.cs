b23347e08b27c847523239898822abb15430f89b
bbiircan <bircanbircan1963@gmail.com>
2025-02-26 15:52:02 +0300

    Add project files.

---
+++ ApiProjectCamp.WebApi/Entities/Feature.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProjectCamp.WebApi.Entities
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+        public string UrlVideo { get; set; }
+        public string UrlImage { get; set; }
+    }
+}
