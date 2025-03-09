1c8a15a00f4eb631f3ef0736e6a7dee33df8caaa
bbiircan <bircanbircan1963@gmail.com>
2025-03-09 14:17:28 +0300

    Added product validation rules for name, price, and description in ProductValidator class.

---
+++ ApiProjectCamp.WebApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using ApiProjectCamp.WebApi.Entities;
+using FluentValidation;
+
+namespace ApiProjectCamp.WebApi.ValidationRules
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x=>x.ProductName).NotEmpty().WithMessage("Enter the product name ").MinimumLength(2)
+                .WithMessage("The product name cannot be less than 2 characters").MaximumLength(50)
+                .WithMessage("The product name cannot be more than 50 characters.");
+
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Enter the product price").LessThan(0)
+                .WithMessage("The product price cannot be a negative value").GreaterThan(1000)
+                .WithMessage("The product price is too high, check the value you entered");
+
+            RuleFor(x => x.Content).NotEmpty().WithMessage("Enter the product description");
+        }
+    }
+}
