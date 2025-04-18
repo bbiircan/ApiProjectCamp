424d5e5d8a5550cfbaabfb6b1ed3da8e293c5c4e
bbiircan <bircanbircan1963@gmail.com>
2025-04-18 19:54:15 +0300

    Consumed API and listed service data.

---
+++ ApiProjectCamp.WebApi/Controllers/ServicesController.cs
@@ -0,0 +1,57 @@
+﻿using ApiProjectCamp.WebApi.Context;
+using ApiProjectCamp.WebApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ServicesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateService(Service service)
+        {
+            _context.Services.Add(service);
+            _context.SaveChanges();
+            return Ok("The addition operation was successful.");
+        }
+
+        [HttpGet]
+        public IActionResult ServiceList()
+        {
+            var values = _context.Services.ToList();
+            return Ok(values);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteService(int id)
+        {
+            var value = _context.Services.Find(id);
+            _context.Services.Remove(value);
+            _context.SaveChanges();
+            return Ok("The service has been successfully deleted.");
+        }
+
+        [HttpGet("GetService")]
+        public IActionResult GetService(int id)
+        {
+            var value = _context.Services.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateService(Service service)
+        {
+            _context.Services.Update(service);
+            _context.SaveChanges();
+            return Ok("The service has been successfully updated.");
+        }
+    }
+}
