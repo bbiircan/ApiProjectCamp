b640f47f8795ec321e65ed9c7c4258e95ebbd525
bbiircan <bircanbircan1963@gmail.com>
2025-06-07 18:59:21 +0300

    Improved code structure in Testimonialpage.

---
+++ ApiProjectCamp.WebApi/Controllers/TestimonialsController.cs
@@ -0,0 +1,57 @@
+﻿using ApiProjectCamp.WebApi.Context;
+using ApiProjectCamp.WebApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public TestimonialsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateTestimonial(Testimonial testimonial)
+        {
+            _context.Testimonials.Add(testimonial);
+            _context.SaveChanges();
+            return Ok("The addition operation was successful.");
+        }
+
+        [HttpGet]
+        public IActionResult TestimonialList()
+        {
+            var values = _context.Testimonials.ToList();
+            return Ok(values);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var value = _context.Testimonials.Find(id);
+            _context.Testimonials.Remove(value);
+            _context.SaveChanges();
+            return Ok("The testimonial has been successfully deleted.");
+        }
+
+        [HttpGet("GetTestimonial")]
+        public IActionResult GetTestimonial(int id)
+        {
+            var value = _context.Testimonials.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTestimonial(Testimonial testimonial)
+        {
+            _context.Testimonials.Update(testimonial);
+            _context.SaveChanges();
+            return Ok("The testimonial has been successfully updated.");
+        }
+    }
+}
