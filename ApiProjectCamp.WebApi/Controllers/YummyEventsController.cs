329e3fa37ff295c5bbf43585d80c50f3fb42bb54
bbiircan <bircanbircan1963@gmail.com>
2025-06-08 16:13:42 +0300

    Improved structure and readability of event code.

---
+++ ApiProjectCamp.WebApi/Controllers/YummyEventsController.cs
@@ -0,0 +1,57 @@
+﻿using ApiProjectCamp.WebApi.Context;
+using ApiProjectCamp.WebApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YummyEventsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public YummyEventsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateYummyEvent(YummyEvent yummyEvent)
+        {
+            _context.YummyEvents.Add(yummyEvent);
+            _context.SaveChanges();
+            return Ok("The addition operation was successful.");
+        }
+
+        [HttpGet]
+        public IActionResult YummyEventList()
+        {
+            var values = _context.YummyEvents.ToList();
+            return Ok(values);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteYummyEvent(int id)
+        {
+            var value = _context.YummyEvents.Find(id);
+            _context.YummyEvents.Remove(value);
+            _context.SaveChanges();
+            return Ok("The event has been successfully deleted.");
+        }
+
+        [HttpGet("GetYummyEvent")]
+        public IActionResult GetYummyEvent(int id)
+        {
+            var value = _context.YummyEvents.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateYummyEvent(YummyEvent yummyEvent)
+        {
+            _context.YummyEvents.Update(yummyEvent);
+            _context.SaveChanges();
+            return Ok("The event has been successfully updated.");
+        }
+    }
+}
