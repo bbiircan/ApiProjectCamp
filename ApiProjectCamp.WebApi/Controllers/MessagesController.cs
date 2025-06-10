61d9da729cb33f6d0654b362532e96909cad9c24
bbiircan <bircanbircan1963@gmail.com>
2025-06-10 17:49:04 +0300

    Optimized navbar message list and improved admin panel layout readability.

--- ApiProjectCamp.WebApi/Controllers/MessagesController.cs
+++ ApiProjectCamp.WebApi/Controllers/MessagesController.cs
@@ -59,5 +59,12 @@ namespace ApiProjectCamp.WebApi.Controllers
             _context.SaveChanges();
             return Ok("The message has been successfully updated.");
         }
+
+        [HttpGet("MessageListByIsReadFalse")]
+        public IActionResult MessageListByIsReadFalse()
+        {
+            var value = _context.Messages.Where(x => x.IsRead == false).ToList();
+            return Ok(value);
+        }
     }
 }
